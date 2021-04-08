85c50db090d298acf94cfe0d71ccaceb5064bfbf
bd210 <82154623+bd210@users.noreply.github.com>
2021-04-08 17:35:03 +0200

    Update BaseEntity.cs

--- Domain/BaseEntity.cs
+++ Domain/BaseEntity.cs
@@ -9,5 +9,6 @@ namespace Domain
         public int Id { get; set; }
         public DateTime DateCreated { get; set; }
         public DateTime? DateModified { get; set; }
+        public DateTime? DateDeleted {get;set;}
     }
 }
